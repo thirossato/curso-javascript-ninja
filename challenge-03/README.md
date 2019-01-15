f8ebebf856c5842ddb96cbe956eb94c8c7208bbb
thiago <thiago@krooze.com.br>
2019-01-15 17:05:26 -0200

    desafio 6

--- challenge-03/README.md
+++ challenge-03/README.md
@@ -2,7 +2,7 @@
 
 ```js
 // Declarar uma variável qualquer, que receba um objeto vazio.
-?
+var vazio = {}
 
 /*
 Declarar uma variável `pessoa`, que receba suas informações pessoais.
@@ -16,7 +16,16 @@ As propriedades e tipos de valores para cada propriedade desse objeto devem ser:
 - `andando` - Boolean - recebe "falso" por padrão
 - `caminhouQuantosMetros` - Number - recebe "zero" por padrão
 */
-?
+var pessoa {
+	nome: '',
+	sobrenome: '',
+	sexo: '',
+	idade: 0,
+	altura: 0,
+	peso: 0,
+	andando: false,
+	caminhouQuantosMetros: 0
+}
 
 /*
 Adicione um método ao objeto `pessoa` chamado `fazerAniversario`. O método deve
--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'Campeonato Brasileiro';
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +13,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = [ 'Corinthians', 'Palmeiras', 'Vasco', 'Gremio', 'Fluminense' ];
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,19 +33,32 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+var showTeamPosition = function (position){
+	if(teams.length > --position){
+		return teams[position];
+	}else{
+		return 'Não temos a informação do time que está nessa posição.';
+	}
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+console.log(showTeamPosition(2));
+console.log(showTeamPosition(1));
+console.log(showTeamPosition(5));
+console.log(showTeamPosition(10));
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+var i = 20
+while (i <= 30){
+	i;
+	i++;
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +72,31 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+var convertToHex = function (cor){
+	switch(cor){
+		case 'black':
+			return 'O hexadecimal para a cor '+ cor + ' é ' + '#000000';
+		case 'white':
+			return 'O hexadecimal para a cor '+ cor + ' é ' + '#ffffff';
+		case 'indigo':
+			return 'O hexadecimal para a cor '+ cor + ' é ' + '#4B0082';
+		case 'red':
+			return 'O hexadecimal para a cor '+ cor + ' é ' + '#DC143C	';
+		case 'green':
+			return 'O hexadecimal para a cor '+ cor + ' é ' + '#008000';
+		default:
+			return 'Não temos o equivalente hexadecimal para '+cor;
+	}
+}
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+console.log(convertToHex('black'));
+console.log(convertToHex('blue'));
+console.log(convertToHex('indigo'));
+console.log(convertToHex('green'));
+console.log(convertToHex('white'));
+console.log(convertToHex('red'));
+console.log(convertToHex('gray'));
+console.log(convertToHex('pink'));
